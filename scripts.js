4501168e9810f08cde671525ab92a796139d1d69
zemownkee <zemownkee@gmail.com>
2022-12-19 22:50:02 -0500

    Add basic form validation rules and regex patterns

--- scripts.js
+++ scripts.js
@@ -1,36 +1,50 @@
-let fields = document.querySelectorAll('input');
+
+let fieldsList = document.querySelectorAll('input');
+let fields = Array.from(fieldsList);
 fields.forEach((field) => field.addEventListener('blur', checkValid));
 
 function checkValid(event) {
- if(event.target.value != "") {
-        console.log(event.target);
-        event.target.classList.add('touched');
+    //handle password validation first
+    if(event.target.id == 'confirm') {
+        checkPasswordMatch();
+    }
+    else if(event.target.value != "" && event.target.validity.valid == true) {
+        setValid(event.target);
+    }
+    else if(event.target.value != "" && event.target.validity.valid == false) {
+        setInvalid(event.target);
+    }
+    else if (event.target.value == "" && event.target.required == true) {
+        setInvalid(event.target);
     }
 }
 
+//pass DOM elements as arguments to set styles.
 function setValid(...elements) {
-    for(const element in elements) {
+    for(const element of elements) {
         element.classList.add('valid');
         element.classList.remove('invalid');
     }
 }
 
 function setInvalid(...elements) {
-    for(const element in elements) {
+    for(const element of elements) {
         element.classList.add('invalid');
         element.classList.remove('valid');
     }
 }
 
-let passwordToCheck = document.getElementById('password').value;
-let checkingPassword = document.getElementById('confirm').value;
-let passwordBox = document.getElementById('password');
-let verificationBox = document.getElementById('confirm');
-verificationBox.addEventListener('blur', checkPasswordMatch);
-
-function checkPasswordMatch(event) {
+function checkPasswordMatch() {
+    let passwordToCheck = document.getElementById('password').value;
+    let checkingPassword = document.getElementById('confirm').value;
+    let passwordBox = document.getElementById('password');
+    let verificationBox = document.getElementById('confirm');  
     if(passwordToCheck === checkingPassword) {
        setValid(passwordBox, verificationBox);
     }
     else setInvalid(passwordBox, verificationBox);
+}
+
+function passwordStrength() {
+    
 }
